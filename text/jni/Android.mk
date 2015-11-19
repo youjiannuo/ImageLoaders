bf94916cf0947be5c66c367b5f0f550fcb4360b1
yjn <youjiannuo@maka.im>
2015-11-19 17:48:39 +0800

    提交图片缓存

--- text/jni/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE    := yjn_ndk
-LOCAL_SRC_FILES := com_yjn_text_JniClient.c
-
-include $(BUILD_SHARED_LIBRARY)
--- text/src/com/yjn/text/JniClient.java
+++
@@ -1,7 +0,0 @@
-package com.yjn.text;
-public class JniClient {
-	static {
-		System.loadLibrary("yjn_ndk");
-	}
-public static native int []getFasetBlurPix(int pixs[] , int w, int h , int radius);
-}
